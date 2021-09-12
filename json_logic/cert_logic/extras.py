4f0110486b7a38c1ed432fee354606ea20fb638f
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2021-09-12 02:55:25 +0200

    add all missing JsonLogic operators to CertLogic extras

--- json_logic/cert_logic/extras.py
+++ json_logic/cert_logic/extras.py
@@ -1,8 +1,9 @@
-from ..extras import EXTRAS_ONLY
+from ..extras import EXTRAS as JSONLOGIC_EXTRAS
 from ..types import Operations
-from .builtins import BUILTINS
+from .builtins import BUILTINS, to_bool
 
 EXTRAS: Operations = {
+    **JSONLOGIC_EXTRAS,
+    '!!': lambda data=None, value=None, *_ignored: to_bool(value),
     **BUILTINS,
-    **EXTRAS_ONLY,
 }
